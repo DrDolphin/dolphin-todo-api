d6aa20bba523208a2df5e2266aff89d57720855b
Robert <rcollins@causeview.com>
2023-10-09 20:38:00 -0400

    Updated TodoItem fields

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -1,12 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DolphinTodoWebAPI.Models
 {
     public class TodoItem
     {
         public int Id { get; set; }
-        public required string Title { get; set; }
+        [Required]
+        public string Title { get; set; } = string.Empty;
         public string? Description { get; set; }
         public DateTime? DueDate { get; set; }
         public int DurationInMinutes { get; set; }
+        [Required]
         public int UserId { get; set; }
+        public Boolean Status { get; set; } = false;
+        public int ParentId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     }
 }
